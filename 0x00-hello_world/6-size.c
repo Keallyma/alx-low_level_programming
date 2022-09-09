1004cbb4d021aeca5b48739a60338888ce336bfc
keallyma <ekeshantal@gmail.com>
2022-09-09 15:11:57 -0700

    legs

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Description: File that prints the size of various of text
+ * Description: File that prints the size of various types of text
  * Return: Always (0) success
  */
 int main(void)
 {
-	printf("Sizes of a char: %d byte(s)\n", sizeof(char));
-	printf("Sizes of an int: %d byte(s)\n", sizeof(int));
-	printf("Sizes of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Sizes of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Sizes of a float: %d byte(s)\n", sizeof(float));
+	printf("Sizes of a char: %d byte(s)\n" sizeof(char));
+	printf("Sizes of an int: %d byte(s)\n" sizeof(int));
+	printf("Sizes of a long int: %d byte(s)\n" sizeof(long int));
+	printf("Sizes of a long long int: %d byte(s)\n" sizeof(long long int));
+	printf("Sizes of a float: %d byte(s)\n" sizeof(float));
 	return(0);
 }
