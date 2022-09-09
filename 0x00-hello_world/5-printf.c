8556d594a05194d7feb47e6a0018d4a9c3fadaca
keallyma <ekeshantal@gmail.com>
2022-09-08 21:34:14 -0700

    gete

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
-int main()
+/**
+ * main - Entry point
+ * Description: Print with proper grammar, but the outcome is a piece of art
+ * Return: Always 0 (success)
+ */
+int main(void)
 {
 printf("with proper grammar,but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
