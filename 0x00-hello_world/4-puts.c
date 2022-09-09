cb4be051f727b9be8602e98714942806d1b18ee6
keallyma <ekeshantal@gmail.com>
2022-09-08 20:01:40 -0700

    dest

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	  //string initialisation
+	  char Mystr1[10] = "#!/bin/bash";
+	  char Mystr2[10] = "programming is like building a multilingual puzzle";
+	  puts(Mystr1);
+	  puts(Mystr2); 
+
+	  return 0;
+}
+
