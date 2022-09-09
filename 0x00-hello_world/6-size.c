0b9c7d1bb6efe831d427d26dce19fdb8a58af553
keallyma <ekeshantal@gmail.com>
2022-09-09 14:24:02 -0700

    fit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - print the sizes of various types on the computer it is compiled on
- * Description:C program that prints the size of various of text
- * Return: 0 (sucess)
+ * main - sizes of various types on the computer
+ * Description: File that prints the size of various of text
+ * Return: Always (0) success
  */
 int main(void)
 {
-print f("Sizes of a char: %d byte(s)\n" sizeof(char));
-print f("Sizes of an int: %d byte(s)\n" sizeof(int));
-print f("Sizes of a long int: %d byte(s)\n" sizeof(long int));
-print f("Sizes of a long long int: %d byte(s)\n" sizeof(long long int));
-print f("Sizes of a float: %d byte(s)\n" sizeof(float));
+print f("Sizes of a char: %d byte(s)\n", sizeof(char));
+print f("Sizes of an int: %d byte(s)\n", sizeof(int));
+print f("Sizes of a long int: %d byte(s)\n", sizeof(long int));
+print f("Sizes of a long long int: %d byte(s)\n", sizeof(long long int));
+print f("Sizes of a float: %d byte(s)\n", sizeof(float));
 return(0);
 }
