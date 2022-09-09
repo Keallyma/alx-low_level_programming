63f166ee6f0721f7b10caaa7acdafb8ca52e8dfe
keallyma <ekeshantal@gmail.com>
2022-09-09 15:17:23 -0700

    pet

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,12 @@
  * Description: File that prints the size of various types of text
  * Return: Always (0) success
  */
-int main(void)
+int main (void)
 {
 	printf("Sizes of a char: %d byte(s)\n", sizeof(char));
 	printf("Sizes of an int: %d byte(s)\n", sizeof(int));
 	printf("Sizes of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Sizes of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Sizes of a float: %d byte(s)\n", sizeof(float));
-	return(0);
+	return (0);
 }
