a560679910966d3f5a0cafcdb560c56bba6cd20b
keallyma <ekeshantal@gmail.com>
2022-09-09 18:30:06 -0700

    rest

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,13 @@
 */
 
 int main(void)
-	[
-		int n = 0;
-
-		while (n < 10)
-		{
-			printf("%i, n);
-			n++;
+{
+	int n = 0;
+	
+	while (n < 10)
+	{
+		printf("%i, n);
+		n++;
 		}
 		putchar('\n');
 		return (0);
