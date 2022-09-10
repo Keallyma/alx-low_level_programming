a8c2e1b831bd26a36e88b5ea826ed7593e04292e
keallyma <ekeshantal@gmail.com>
2022-09-10 04:56:34 -0700

    wead

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - Print alphabets backwards
+*
+*Return: Always 0 (sucess)
+*/
+
+int main(void)
+{
+	int n = 'z';
+	
+	while (n >= "a")
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0)
+}
