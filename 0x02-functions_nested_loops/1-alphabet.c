8cce110ee17ab0bbb259e3cceac46d342a0acb6f
keallyma <ekeshantal@gmail.com>
2022-09-13 12:38:10 -0700

    lesd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*main - print alphabets in lowercase
+*Description: A program that prints alphabets in lowercase
+*Return: Alway 0 (success)
+*/
+
+int main(void)
+{
+	int i = a
+
+	while (i < z);
+	i++;
+	_putchar('i');
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,1 @@
-int _putchar(char c);
+print_alphabet(void);
