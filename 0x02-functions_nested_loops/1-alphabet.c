1b19851cbc005e212223a497d50d98e331098c66
keallyma <ekeshantal@gmail.com>
2022-09-13 14:55:45 -0700

    her

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,12 @@
 
 void print_alphabet(void)
 {
-	char i = 'a';
+	char alphabet = 'a';
 
-	while (i <= 'z')
+	while (alphabet <= 'z')
 	{
-		_putchar('i');
-		i++;
+		_putchar('alphabet');
+		alphabet++;
 	}
 	_putchar('\n');
 }
