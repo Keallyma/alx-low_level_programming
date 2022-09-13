205b7ce730960f34702bd9036d0820716f1d0643
keallyma <ekeshantal@gmail.com>
2022-09-13 16:03:10 -0700

    legs

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,19 @@
 #include <main.h>
 
 /**
- * main - print alphabets ten times
+ * print_alphabet_x10 - print alphabets ten times
  *
  *
  */
 
 void print_alphabet_x10(void);
 {
-
+	char alphat = 'a'
+	
+	while (alphat <= 'z')
+	{
+		_putchar (alphat);
+		alphat++;
+	}
+	_putchar('10\n');
+}
