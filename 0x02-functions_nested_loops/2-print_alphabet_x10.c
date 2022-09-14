8567ab34cfc87441e781cfbb72801012c86a579f
keallyma <ekeshantal@gmail.com>
2022-09-14 04:14:08 -0700

    sea

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 {
 	char i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i < '10'; i++)
 	{
-	       for (j = 'a'; j <= 'z'; j++)
+	for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
