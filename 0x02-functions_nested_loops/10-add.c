d11201a6d2e7055bd2c74c827e2770d4fe667209
keallyma <ekeshantal@gmail.com>
2022-09-14 11:09:11 -0700

    lasd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *add - add two numbers
+ *
+ *Return: a plus b
+ *@a: first number
+ *@b: second number
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *jack-bauer - print number of time
+ *Description: prints a list of time
+ *Return: Always 0 (success)
+ */
+
+void jack_bauer(void)
+{
+	int i, t;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (t = 0; t < 60; t++)
+		{
+			_putchar(i / 10 + 48);
+			_putchar(i % 10 + 48);
+			_putchar(':');
+			_putchar(t / 10 + 48);
+			_putchar(t % 10 + 48);
+			_putchar('\n');
+		}
+	}
+}
