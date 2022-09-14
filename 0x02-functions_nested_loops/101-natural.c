4e08d9cc1d932d9dac11da56b3bd83ebb3994c5a
keallyma <ekeshantal@gmail.com>
2022-09-14 14:17:10 -0700

    kels

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,12 +13,10 @@ int main(void)
 
 	end_num = 1024;
 	total = 0;
-	
 	for (start_num = 0; start_num < end_num; start_num++)
 	{
 		if ((start_num % 3 == 0) || (start_num % 5 == 0))
 		{
-		
 			total = total + start_num;
 		}
 		else
