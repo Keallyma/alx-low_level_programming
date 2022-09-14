f834d8fada401113fef2b33f5e272afab269bf68
keallyma <ekeshantal@gmail.com>
2022-09-14 05:54:46 -0700

    kel

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - to check for lowercase character
  * Description: A function that checks for lowercases
- * Return: 1 if 'c' is letter lowercase or uppercase otherwise 0
+ * Return: 1 if c is a letter otherwise 0
  */
 
 int _islower(int c)
