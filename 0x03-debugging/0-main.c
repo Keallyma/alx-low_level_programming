99982d5f76faf6382d6abdfb2ff207263a1a52d4
keallyma <ekeshantal@gmail.com>
2022-09-15 06:27:12 -0700

    jet

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,6 +2,7 @@
 
 /**
  *main - tests function thta prints if integer is positive or negative
+ *
  *Return: 0
  */
 
@@ -9,7 +10,7 @@ Int main(void)
 {
 	int i;
 
-	i = 98
+	i = 98;
 	positive_or_negative(i);
 
 	return (0);
