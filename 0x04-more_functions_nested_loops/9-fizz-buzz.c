67d9e31f745d7f58e2480fd0cac209cc2afbea8f
keallyma <ekeshantal@gmail.com>
2022-09-16 20:19:15 -0700

    dragon

---
+++ 0x04-more_functions_nested_loops/9-fizz-buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - print Buzz each number of three and five
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf (" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
