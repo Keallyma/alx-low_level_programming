dec2253aa5ec9db023fc8915251a6675b5740f42
keallyma <ekeshantal@gmail.com>
2022-09-16 18:29:49 -0700

    jetu

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers
+ * (neither 2 nor 4)
+ * Return: no return
+ */
+
+void print_most_numbers(void)
+{
+	Int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		if (ch != 50 && ch != 52)
+		{
+			_putchar(ch);
+		}
+	_putchar('\n');
+}
