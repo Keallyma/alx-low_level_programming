77fdceb8f27bbc55e4110b841f011b136416206e
keallyma <ekeshantal@gmail.com>
2022-09-16 19:28:41 -0700

    hearsd

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_line - print straight line n times.
- * @n: times straight line is printed.
- * Return: no return.
+ * print_line - print straight line n times
+ * @n: input times straight line is printed
+ * Return: no return
  */
 
 void print_line(int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		_putchar(95);
