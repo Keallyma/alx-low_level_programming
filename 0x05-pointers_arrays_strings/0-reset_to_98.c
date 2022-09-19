3e274ee11658fae20e75c8f3377e1ad74a025d71
keallyma <ekeshantal@gmail.com>
2022-09-19 07:28:01 -0700

    strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - take a pointer to an int as parameter
+ * update the value it points to to 98.
+ * Return: no return.
+ */
+
+void reset_to_98(int *n)
+{
+	int n;
+	int *p;
+
+	p = &n;
+	n = 98;
+	printf("Value of 'n' before the call: %d\n", n);
+	printf("Address of 'n': %p\n", &n);
+	printf("Value of 'p': %p\n", p);
+	printf("Address of 'p': %p\n", &p);
+	modif_my_param(p);
+	printf("Value of 'n' after the call: %d\n", n);
+
+	_putchar('\n');
+}
