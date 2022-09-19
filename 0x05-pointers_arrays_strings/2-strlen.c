2ac83f789479b4e655052738c01b8ee9ab9f6c6c
keallyma <ekeshantal@gmail.com>
2022-09-19 14:53:19 -0700

    keal

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * strlen - return the length of a string
+ * @s: value of length
+ * Return: always 0 (success)
+ */
+
+int _strlen(char *s)
+{
+	_putchar("strlen(*s) : %d\n", strlen);
+	return (0);
+}
