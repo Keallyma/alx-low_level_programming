4a324b614c5fb0762c21f12785c2a8f7dd2be134
keallyma <ekeshantal@gmail.com>
2022-09-19 07:32:30 -0700

    jet

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * reset_to_98 - take a pointer to an int as parameter
@@ -8,7 +9,7 @@
 
 void reset_to_98(int *n)
 {
-	int n;
+	int *n;
 	int *p;
 
 	p = &n;
