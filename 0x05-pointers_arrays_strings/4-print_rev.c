34030bfd521cffb030fc328cf69be9e267868da3
keallyma <ekeshantal@gmail.com>
2022-09-19 17:16:27 -0700

    lead

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse, followed by a new line
+ * @s: 
+ * Return: no return
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		_putchar(s[c]);
+		c--;
+	}
+	_putchar('\n');
+}
