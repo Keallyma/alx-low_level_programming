00d660200091cd5265d47440ecd46208458c7672
keallyma <ekeshantal@gmail.com>
2022-09-19 17:55:04 -0700

    peas

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,17 +2,17 @@
 
 /**
  * print_rev - print string in reverse, followed by a new line
- * @s: string being printed 
+ * @s:string being printed 
  * Return: no return
  */
 
 void print_rev(char *s)
 {
 	int c = 0;
-       
+	
 	while ((s[c] / 2) != '\0')
 	{
-		_putchar(s[c]);
+		_putchar(s[c] - 2);
 		c--;
 	}
 	_putchar('\n');
