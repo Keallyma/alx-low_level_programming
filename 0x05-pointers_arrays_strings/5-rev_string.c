e2df7fab11c8d9f3d88c0e96f8cbbce90d1833da
keallyma <ekeshantal@gmail.com>
2022-09-19 19:33:37 -0700

    yes

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string>
+
+/**
+ * rev_string - reverse string
+ * @s: being string printed reversely
+ * Return: c return
+ */
+
+void rev_string(char *s)
+{
+	int c, i;
+
+	c = strlen(s);
+	for (i = (c - 1); i >= 0; i--)
+	
+	return (c);
+}
