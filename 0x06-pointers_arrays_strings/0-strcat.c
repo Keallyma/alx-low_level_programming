b64e65cad7ea86495be3747e87cfcb2bbcb913ab
keallyma <ekeshantal@gmail.com>
2022-09-21 15:25:00 -0700

    heal

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,19 +15,19 @@ char *_strcat(char *dest, char *src)
 	int length, j;
 	
 	length = 0
-	while (dest[length] != '\0') 
+	while (*dest[length] != '\0') 
 	{
 	++length;
 	}
 
-	for (j = 0; src[j] != '\0'; ++j, ++length)
+	for (j = 0; *src[j] != '\0'; ++j, ++length)
 	{
-	dest[length] = src[j];
+	*dest[length] = *src[j];
 	}
-	dest[length] = '\0';
+	*dest[length] = '\0';
 
 	printf("After concatenation: ");
-	puts(dest);
+	puts(*dest);
 
 	return (0);
 }
