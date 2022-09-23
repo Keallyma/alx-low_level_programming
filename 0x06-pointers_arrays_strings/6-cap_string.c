8556f5bbf3af5e22745c9aac57d545d020dd44df
keallyma <ekeshantal@gmail.com>
2022-09-23 07:56:21 -0700

    yet

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -23,7 +23,8 @@ char *cap_string(char *s)
 		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
 		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
 		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
-		    || s[count] == '"' || s[count] == '(' || s[count] == ')'			    || s[count] == '{' || s[count] == '}')
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+		    || s[count] == '{' || s[count] == '}')
 		{
 			if (s[count + 1] >= 97 && s[count + 1] <= 122)
 			{
