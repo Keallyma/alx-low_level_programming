df9692b0837e7879ebee08a6d784e4653346e0b6
keallyma <ekeshantal@gmail.com>
2022-09-22 19:47:28 -0700

    het

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * cap_string - capitalize every first letetr of a word in a string.
- * separators of words are: space, tabulation,
- * new line, ,, ;, ., !, ?, ", (, ), {, and }.
- * @s: pointer to string.
+ * cap_string - capitalize every first letetr of a word in a string
+ * separators of words are: space, tabulation
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }
+ * @s: pointer to string
  *
- * Return: pointer to s.
+ * Return: pointer to s
  */
 
 char *cap_string(char *s)
@@ -21,9 +21,9 @@ char *cap_string(char *s)
 			s[0] = s[0] - 32;
 		}
 		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
-			|| s[count] == ',' || s[count] == ';' || s[count] == '.'
-			|| s[count] == '.' || s[count] == '!' || s[count] == '?'
-			|| s[count] == '"' || s[count] == '(' || s[count] == ')'			|| s[count] == '{' || s[count] == '}')
+		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'			    || s[count] == '{' || s[count] == '}')
 		{
 			if (s[count + 1] >= 97 && s[count + 1] <= 122)
 			{
