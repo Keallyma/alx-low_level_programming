21a2791ae7eb96b392812be591b376472255e901
keallyma <ekeshantal@gmail.com>
2022-09-23 08:14:09 -0700

    let

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337
- * Letters a and A are replaced by 4
- * Letters e and E are replaced by 3
- * Letters o and O are replaced by 0
- * Letters t and T are replaced by 7
- * Letters l and L are replaced by 1
- * @s: pointer to string
+ * leet - encodes a string into 1337.
+ * Letters a and A are replaced by 4,
+ * Letters e and E are replaced by 3,
+ * Letters o and O are replaced by 0,
+ * Letters t and T are replaced by 7,
+ * Letters l and L are replaced by 1.
+ * @s: pointer to string.
  *
- * Return: pointer to s 
+ * Return: pointer to s.
  */
 
 char *leet(char *s)
@@ -32,5 +32,6 @@ char *leet(char *s)
 		}
 		stringCount++;
 	}
+
 	return (s);
 }
