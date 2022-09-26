22a07764c0b19325d0f90e8a85c3883fef68c91e
keallyma <ekeshantal@gmail.com>
2022-09-26 13:26:23 -0700

    more strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_memset - fill memory with a constant byte
+ * @b: va;ue of constant byte
+ * @n: value for number of bytes
+ * Return: nothing
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int n;
+
+	n = 0;
+	while (n < s)
+	{
+		if (n % 10)
+		{
+			_printf(" ");
+		}
+		if (!(n % 10) && n)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[n]);
+		n++;
+	}
+	printf("\n");
+}
