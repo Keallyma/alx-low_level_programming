a4f403f45c8c55843ed5144fb16862f05f512218
keallyma <ekeshantal@gmail.com>
2022-09-26 14:04:14 -0700

    real

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,28 +2,19 @@
 
 /**
  * *_memset - fill memory with a constant byte
- * @b: va;ue of constant byte
+ * @s: pointer memory area
+ * @b: value of constant byte
  * @n: value for number of bytes
- * Return: nothing
+ * Return: pointer to memory area s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int n;
+	unsigned int counter;
 
-	n = 0;
-	while (n < s)
+	for (counter = 0; counter < n; counter++)
 	{
-		if (n % 10)
-		{
-			_printf(" ");
-		}
-		if (!(n % 10) && n)
-		{
-			printf("\n");
-		}
-		printf("0b%02b", buffer[n]);
-		n++;
+		s[counter] = b;
 	}
-	printf("\n");
+	return (s);
 }
