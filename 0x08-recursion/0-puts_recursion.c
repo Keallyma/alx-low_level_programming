8a2e9c093d54bb30886f60d793c55ade616fd966
keallyma <ekeshantal@gmail.com>
2022-09-28 10:59:47 -0700

    yers

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string follow by a new line
+ * @s: funtion input
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("_");
+	printf("p");
+	printf("u");
+	printf("t");
+	printf("c");
+	printf("h");
+	printf("a");
+	printf("r");
+	printf("\n");
+}
+
+int main(void)
+{
+	_puts_recursion("_putchar\n");
+}
