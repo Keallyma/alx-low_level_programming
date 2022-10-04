ab39d3d1ac62e117e28337b9827eb1ace881fa79
keallyma <ekeshantal@gmail.com>
2022-10-04 15:56:57 -0700

    jet

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -44,11 +44,11 @@ char *str_concat(char *s1, char *s2)
 	}
 
 	for (i = 0; *(s1 + i) != '\0'; i++)
-		*(dst + 1) = *(s1 + i);
+		*(dst + i) = *(s1 + i);
 
 	for (j = 0; *(s2 + j) != '\0'; j++);
 	{
-
+		
 		*(dst + i) = *(s2 + j);
 		i++;
 	}
